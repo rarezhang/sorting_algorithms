b559c9e1eb565b095e78b8c94fa9a2ea230e930d
rarezhang <catezhang@qq.com>
2017-02-05 05:31:16 +0000

    insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -17,12 +17,14 @@ def insertion_sort(L):
     """
     assert isinstance(L, list)
     length = len(L)
+    # outer loop: runs over all the elements except the first one (1st element already sorted)
     for i in range(1, length):  # start with the 2nd element
         current_value = L[i]
-        position = i
-        while position>0 and L[position-1]>current_value: # find appropriate position for insertion
-            L[position] = L[position-1]
-            position -= 1
-        L[position] = current_value
+        j = i-1
+        # inner loop: moves element L[i] to its correct place so that after the loop, the first i+2 elements are sorted.
+        while j>=0 and L[j]>current_value:  # find appropriate position for insertion
+            L[j+1] = L[j]
+            j -= 1
+        L[j+1] = current_value
     return L
 
